ab0d413776480550b564fd11a55c0f72ea62a32d
SachiniDIL <sdilrangi58@gmail.com>
2024-04-18 14:05:00 +0530

    commented all

--- src/main/java/com/happyman/Ruby/packages/utils/PackageAndAddonList.java
+++ src/main/java/com/happyman/Ruby/packages/utils/PackageAndAddonList.java
@@ -1,32 +1,32 @@
-package com.happyman.Ruby.packages.utils;
-
-import com.happyman.Ruby.masterService.MasterService;
-import com.happyman.Ruby.masterService.dao.Package;
-import com.happyman.Ruby.packages.dto.PackageDTO;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class PackageAndAddonList {
-
-    public static List<PackageDTO> packageDTOList(MasterService masterService)
-    {
-        List<PackageDTO> packageDTOList = new ArrayList<>();
-        List<Package> packages = masterService.getAllPackages();
-        for(Package p : packages)
-        {
-            PackageDTO packageDTO = new PackageDTO();
-            packageDTO.setId(p.getId());
-            packageDTO.setPackageName(p.getName());
-            packageDTO.setPackageDescription(p.getDescription());
-            packageDTO.setPackagePrice(p.getPrice());
-            packageDTO.setPackageDiscontinueDate(p.getDiscontinueDate());
-            packageDTO.setPackageAvailability(p.getAvailability());
-            packageDTO.setPackageType(p.getType().toString());
-            packageDTO.setMaxAdults(p.getMaxAdults());
-            packageDTO.setAddonList(masterService.getAddonByPackageId(p.getId()));
-            packageDTOList.add(packageDTO);
-        }
-        return packageDTOList;
-    }
-}
+//package com.happyman.Ruby.packages.utils;
+//
+//import com.happyman.Ruby.masterService.MasterService;
+//import com.happyman.Ruby.masterService.dao.Package;
+//import com.happyman.Ruby.packages.dto.PackageDTO;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class PackageAndAddonList {
+//
+//    public static List<PackageDTO> packageDTOList(MasterService masterService)
+//    {
+//        List<PackageDTO> packageDTOList = new ArrayList<>();
+//        List<Package> packages = masterService.getAllPackages();
+//        for(Package p : packages)
+//        {
+//            PackageDTO packageDTO = new PackageDTO();
+//            packageDTO.setId(p.getId());
+//            packageDTO.setPackageName(p.getName());
+//            packageDTO.setPackageDescription(p.getDescription());
+//            packageDTO.setPackagePrice(p.getPrice());
+//            packageDTO.setPackageDiscontinueDate(p.getDiscontinueDate());
+//            packageDTO.setPackageAvailability(p.getAvailability());
+//            packageDTO.setPackageType(p.getType().toString());
+//            packageDTO.setMaxAdults(p.getMaxAdults());
+//            packageDTO.setAddonList(masterService.getAddonByPackageId(p.getId()));
+//            packageDTOList.add(packageDTO);
+//        }
+//        return packageDTOList;
+//    }
+//}
