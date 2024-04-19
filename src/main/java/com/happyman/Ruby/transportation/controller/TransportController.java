c1ab4b72995b373b6554ec6141c18622c2dc39a9
Silverviles <sudarakatharindu2001@gmail.com>
2024-04-19 03:50:57 +0000

    Fixed jquery

--- src/main/java/com/happyman/Ruby/transportation/controller/TransportController.java
+++ src/main/java/com/happyman/Ruby/transportation/controller/TransportController.java
@@ -81,4 +81,9 @@ public class TransportController extends BaseController {
 	public String getLoginForm() {
 		return "transportation/driver_login";
 	}
+
+	@GetMapping("/transportForm")
+	public String getTransportForm(){
+		return "transportation/transportForm";
+	}
 }
