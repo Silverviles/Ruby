98e680b9641f96dd8fce837d009aead2d0ec7bf3
Silverviles <sudarakatharindu2001@gmail.com>
2024-04-22 16:24:13 +0000

    Room Reservation Service Complete

--- src/main/java/com/happyman/Ruby/masterService/service/RoomReservationService.java
+++ src/main/java/com/happyman/Ruby/masterService/service/RoomReservationService.java
@@ -2,6 +2,7 @@ package com.happyman.Ruby.masterService.service;
 
 import com.happyman.Ruby.masterService.dao.RoomReservation;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface RoomReservationService {
@@ -13,4 +14,10 @@ public interface RoomReservationService {
     List<RoomReservation> getAllRoomReservations();
 
     List<RoomReservation> getAllRoomReservationsByRoomId(Integer roomId);
+
+    List<RoomReservation> getAllRoomReservationsByStartDate(LocalDate startDate);
+
+    List<RoomReservation> getAllRoomReservationsByEndDate(LocalDate endDate);
+
+    List<RoomReservation> getAllRoomReservationsByStartEndDate(LocalDate startDate, LocalDate endDate);
 }
---
+++ src/main/java/com/happyman/Ruby/masterService/service/RoomReservationServiceImpl.java
@@ -0,0 +1,62 @@
+package com.happyman.Ruby.masterService.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.happyman.Ruby.masterService.dao.RoomReservation;
+import com.happyman.Ruby.masterService.repository.RoomReservationRepository;
+
+@Service
+public class RoomReservationServiceImpl implements RoomReservationService {
+
+	private final RoomReservationRepository roomReservationRepository;
+
+	@Autowired
+	public RoomReservationServiceImpl(RoomReservationRepository roomReservationRepository) {
+		this.roomReservationRepository = roomReservationRepository;
+	}
+
+	@Override
+	public void saveRoomReservation(RoomReservation roomReservation) {
+		roomReservationRepository.save(roomReservation);
+	}
+
+	@Override
+	public RoomReservation getRoomReservationById(Integer roomReservationId) {
+		return roomReservationRepository.findById(roomReservationId).orElse(null);
+	}
+
+	@Override
+	public List<RoomReservation> getAllRoomReservations() {
+		return roomReservationRepository.findAll();
+	}
+
+	@Override
+	public List<RoomReservation> getAllRoomReservationsByRoomId(Integer roomId) {
+		return getAllRoomReservations().stream().filter(
+			roomReservation -> roomReservation.getRoom().getId().equals(roomId)).toList();
+	}
+
+	@Override
+	public List<RoomReservation> getAllRoomReservationsByStartDate(LocalDate startDate) {
+		return getAllRoomReservations().stream().filter(
+			roomReservation -> roomReservation.getStartDate().equals(startDate)).toList();
+	}
+
+	@Override
+	public List<RoomReservation> getAllRoomReservationsByEndDate(LocalDate endDate) {
+		return getAllRoomReservations().stream().filter(
+			roomReservation -> roomReservation.getEndDate().equals(endDate)).toList();
+	}
+
+	@Override
+	public List<RoomReservation> getAllRoomReservationsByStartEndDate(LocalDate startDate, LocalDate endDate) {
+		return getAllRoomReservations().stream().filter(
+			roomReservation -> roomReservation.getStartDate().equals(startDate) && roomReservation.getEndDate().equals(
+				endDate)).toList();
+	}
+}
+
