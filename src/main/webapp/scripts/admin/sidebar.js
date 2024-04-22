9ab32f9d9f5e0e7ff06c6565581299faee720d6a
Silverviles <sudarakatharindu2001@gmail.com>
2024-04-22 14:11:20 +0000

    Add div resolve js

--- src/main/webapp/scripts/admin/sidebar.js
+++ src/main/webapp/scripts/admin/sidebar.js
@@ -16,4 +16,20 @@ $(document).ready(function() {
         $('.sub-contents').hide();
         $('#' + roomId).css('display', 'block');
     });
+
+    const showDivId = getUrlParameter('showDiv');
+
+    if (showDivId) {
+        $("#" + showDivId).show();
+    }
+
+    history.replaceState({}, document.title, window.location.pathname);
+
+    function getUrlParameter(name) {
+        const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+            results = regex.exec(window.location.href);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    }
 });
