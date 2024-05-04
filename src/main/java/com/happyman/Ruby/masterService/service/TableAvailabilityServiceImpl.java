1d423e73d7d438049429538d9b9114cd7ba36bbf
Dilshan Kavinda <kavindakgd@gmail.com>
2024-05-04 07:31:22 +0530

    services updated

--- src/main/java/com/happyman/Ruby/masterService/service/TableAvailabilityServiceImpl.java
+++ src/main/java/com/happyman/Ruby/masterService/service/TableAvailabilityServiceImpl.java
@@ -2,18 +2,33 @@ package com.happyman.Ruby.masterService.service;
 
 import com.happyman.Ruby.masterService.dao.TableAvailability;
 import com.happyman.Ruby.masterService.repository.TableAvailabilityRepository;
-import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class TableAvailabilityServiceImpl implements TableAvailabilityService{
     private final TableAvailabilityRepository tableAvailabilityRepository;
 
+    @Autowired
     public TableAvailabilityServiceImpl(TableAvailabilityRepository tableAvailabilityRepository) {
         this.tableAvailabilityRepository = tableAvailabilityRepository;
     }
 
-    public @NonNull List<TableAvailability> getAllSeats() {
+    @Override
+    public List<TableAvailability> getAllSeats() {
         return tableAvailabilityRepository.findAll();
+    }
+
+    @Override
+    public TableAvailability getTableAvailabilityById() {
+        return null;
+    }
+
+    @Override
+    public Optional<TableAvailability> getTableAvailabilityById(Integer id){
+        return tableAvailabilityRepository.findById(id);
     }
 }
